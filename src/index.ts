6c31a097e98fce7e35e3700169102a1b01bdd78d
conde <andersonvilela.al@gmail.com>
2022-12-15 07:17:56 -0300

    usei o mongodb gratis e o conectei

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { request } from 'express';
 import mongoose from 'mongoose';
 import { router } from './router';
 import path from 'node:path';
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb+srv://dev:b0Q1oG1pm6Zai14t@cluster0.zox8ino.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
 
     const app = express();
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017')
     app.use(express.json());
     app.use(router);
 
-    app.listen(3001, () => {
+    app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
 
